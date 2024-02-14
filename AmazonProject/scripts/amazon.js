d6a478b6c8a115ca3a95745401db9e9e5a339d39
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-02-14 13:48:45 +0200

    Make the cart quantity interactive

--- AmazonProject/scripts/amazon.js
+++ AmazonProject/scripts/amazon.js
@@ -54,8 +54,8 @@ products.forEach((product) => {
   `;
 });
 
-document.querySelector('.js-products-grid').
-innerHTML = productsHTML;
+document.querySelector('.js-products-grid')
+  .innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
@@ -79,6 +79,13 @@ document.querySelectorAll('.js-add-to-cart')
         });
       }
 
-      console.log(cart);
+      let cartQuantity = 0;
+
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
     });
   });
