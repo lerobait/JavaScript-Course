349d014ebdf08e7438a4617361b560bc47879317
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-03-25 14:59:08 +0200

    15l

--- HomeWork/Lesson15-Ex-ExternLibs-MVC/15h-n/scripts/checkout/orderSummary.js
+++ HomeWork/Lesson15-Ex-ExternLibs-MVC/15h-n/scripts/checkout/orderSummary.js
@@ -8,7 +8,7 @@ import {
 import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption, calculateDeliveryDate } from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
 import { renderCheckoutHeader } from './checkoutHeader.js';
 
@@ -24,14 +24,7 @@ export function renderOrderSummary() {
 
     const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    const today = dayjs();
-    const deliveryDate = today.add(
-      deliveryOption.deliveryDays,
-      'days'
-    );
-    const dateString = deliveryDate.format(
-      'dddd, MMMM, D'
-    );
+    const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML += `
       <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -85,14 +78,7 @@ export function renderOrderSummary() {
       let html = '';
 
       deliveryOptions.forEach((deliveryOption) => {
-        const today = dayjs();
-        const deliveryDate = today.add(
-          deliveryOption.deliveryDays,
-          'days'
-        );
-        const dateString = deliveryDate.format(
-          'dddd, MMMM, D'
-        );
+        const dateString = calculateDeliveryDate(deliveryOption)
 
         const priceString = deliveryOption.priceCents === 0
           ? 'Free'
