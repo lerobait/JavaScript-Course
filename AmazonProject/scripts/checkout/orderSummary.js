f0a03efb9083f5aee7bc73df2dc31379cc713fc6
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-03-30 14:23:03 +0200

    Added several modifications
    - Replace DOM with renderOrderSummary()
    
    - Replace DOM with renderCheckoutHeader() using MVC
    
    - Reuse code with calculateDeliveryDate(deliveryOption)

--- AmazonProject/scripts/checkout/orderSummary.js
+++ AmazonProject/scripts/checkout/orderSummary.js
@@ -8,8 +8,9 @@ import {
 import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption, calculateDeliveryDate } from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
+import { renderCheckoutHeader } from './checkoutHeader.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML = '';
@@ -23,14 +24,7 @@ export function renderOrderSummary() {
 
     const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    const today = dayjs();
-    const deliveryDate = today.add(
-      deliveryOption.deliveryDays,
-      'days'
-    );
-    const dateString = deliveryDate.format(
-      'dddd, MMMM, D'
-    );
+    const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML += `
       <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -84,14 +78,7 @@ export function renderOrderSummary() {
       let html = '';
 
       deliveryOptions.forEach((deliveryOption) => {
-        const today = dayjs();
-        const deliveryDate = today.add(
-          deliveryOption.deliveryDays,
-          'days'
-        );
-        const dateString = deliveryDate.format(
-          'dddd, MMMM, D'
-        );
+        const dateString = calculateDeliveryDate(deliveryOption)
 
         const priceString = deliveryOption.priceCents === 0
           ? 'Free'
@@ -131,12 +118,9 @@ export function renderOrderSummary() {
           const productId = link.dataset.productId;
           removeFromCart(productId);
 
-          const container = document.querySelector(
-            `.js-cart-item-container-${productId}`
-          );
-          container.remove();
-
+          renderCheckoutHeader();
           updateCartQuantity();
+          renderOrderSummary();
           renderPaymentSummary();
         });
       });
