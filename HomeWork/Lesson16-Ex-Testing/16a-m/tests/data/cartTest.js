bbfb4d89f79e9991fc9b0d3ea23e21327570cf5a
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-04-04 18:51:58 +0300

    16c

--- HomeWork/Lesson16-Ex-Testing/16a-m/tests/data/cartTest.js
+++ HomeWork/Lesson16-Ex-Testing/16a-m/tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import { addToCart, cart, loadFromStorage } from '../../data/cart.js';
+import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
 
 describe('test suite: addToCart', () => {
   it('adds an existing product to the cart', () => {
@@ -16,6 +16,11 @@ describe('test suite: addToCart', () => {
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }]));
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart[0].qantity).toEqual(2);
   });
@@ -27,11 +32,11 @@ describe('test suite: addToCart', () => {
       return JSON.stringify([]);
     });
     loadFromStorage();
-
+    
     addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
-    expect(cart[0].qantity).toEqual(1);
+    expect(cart[0].quantity).toEqual(1);
   });
 });
