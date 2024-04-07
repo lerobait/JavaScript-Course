0f968185494e1b65c4b7217f9aa3dfed6c05e3c0
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-04-07 13:53:28 +0300

    17a

---
+++ HomeWork/Lesson17-Ex-OOP/17a-j/data/car.js
@@ -0,0 +1,21 @@
+class Car {
+  brand;
+  model;
+
+  constructor(carDetails) {
+    this.brand = carDetails.brand;
+    this.model = carDetails.model;
+  }
+}
+
+const car1 = new Car({
+  brand: 'Dodge',
+  model: 'Durango'
+});
+const car2 = new Car({
+  brand: 'Jeep',
+  model: 'Grand Cherokee'
+});
+
+console.log(car1);
+console.log(car2);
