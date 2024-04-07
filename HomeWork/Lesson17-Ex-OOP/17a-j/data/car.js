37b5e69cb63351a7bd9c8916090559938a29f2e2
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-04-07 14:11:24 +0300

    17c

--- HomeWork/Lesson17-Ex-OOP/17a-j/data/car.js
+++ HomeWork/Lesson17-Ex-OOP/17a-j/data/car.js
@@ -5,10 +5,29 @@ class Car {
   constructor(carDetails) {
     this.brand = carDetails.brand;
     this.model = carDetails.model;
+    this.speed = 0;
   }
 
   displayInfo() {
-    console.log(`${this.brand} ${this.model}`);
+    console.log(
+      `${this.brand} ${this.model}, Speed; ${this.speed} kh/m`
+    );
+  }
+
+  go() {
+    this.speed += 5;
+  
+    if (this.speed > 200) {
+      this.speed = 200;
+    }
+  }
+  
+  brake() {
+    this.speed -= 5;
+  
+    if (this.speed < 0) {
+      this.speed = 0;
+    }
   }
 }
 
@@ -25,4 +44,14 @@ console.log(car1);
 console.log(car2);
 
 car1.displayInfo();
+car1.go();
+car1.go();
+car1.go();
+car1.brake();
+car1.displayInfo();
+
+car2.displayInfo();
+car2.go();
+car2.brake();
+car2.brake();
 car2.displayInfo();
