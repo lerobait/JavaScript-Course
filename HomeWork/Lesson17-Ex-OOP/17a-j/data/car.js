deb473fe775a257a6874b182a44d960301edd9e8
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-04-07 14:22:09 +0300

    17d

--- HomeWork/Lesson17-Ex-OOP/17a-j/data/car.js
+++ HomeWork/Lesson17-Ex-OOP/17a-j/data/car.js
@@ -6,16 +6,21 @@ class Car {
     this.brand = carDetails.brand;
     this.model = carDetails.model;
     this.speed = 0;
+    this.isTrunkOpen = false;
   }
 
   displayInfo() {
+    const trunkStatus = this.isTrunkOpen ? 'open' : 'closed';
+
     console.log(
-      `${this.brand} ${this.model}, Speed; ${this.speed} kh/m`
+      `${this.brand} ${this.model}, Speed; ${this.speed} kh/m, Trunk: ${trunkStatus}`
     );
   }
 
   go() {
-    this.speed += 5;
+    if (!this.isTrunkOpen) {
+      this.speed += 5;
+    }
   
     if (this.speed > 200) {
       this.speed = 200;
@@ -28,6 +33,16 @@ class Car {
     if (this.speed < 0) {
       this.speed = 0;
     }
+  }
+
+  openTrunk() {
+    if (this.speed === 0) {
+      this.isTrunkOpen = true;
+    } 
+  }
+
+  closeTrunk() {
+    this.isTrunkOpen = false;
   }
 }
 
@@ -50,8 +65,15 @@ car1.go();
 car1.brake();
 car1.displayInfo();
 
+car1.openTrunk();
+car1.displayInfo();
+
 car2.displayInfo();
 car2.go();
 car2.brake();
 car2.brake();
+car2.displayInfo();
+
+car2.openTrunk();
+car2.go();
 car2.displayInfo();
