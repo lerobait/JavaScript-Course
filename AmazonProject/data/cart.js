7c528898adbb129dd2b717bc9c9ea6a16f5e0147
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-07-01 13:34:00 +0300

    Learn promises and covert code to promises

--- AmazonProject/data/cart.js
+++ AmazonProject/data/cart.js
@@ -3,46 +3,47 @@ export let cart;
 loadFromStorage();
 
 export function loadFromStorage() {
-  cart = JSON.parse(localStorage.getItem('cart'));
+  cart = JSON.parse(localStorage.getItem("cart"));
 
   if (!cart) {
-    cart = [{
-      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 2,
-      deliveryOptionId: '1'
-    }, {
-      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 1,
-      deliveryOptionId: '2'
-    }];
+    cart = [
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: "2",
+      },
+    ];
   }
 }
 
 function saveToStorage() {
-  localStorage.setItem('cart', JSON.stringify(cart));
+  localStorage.setItem("cart", JSON.stringify(cart));
 }
 
 export function addToCart(productId) {
   let matchingItem;
-  
+
   let addedMessageTimeoutId;
 
-  const addedMessage = document.querySelector(
-    `.js-added-to-cart-${productId}`
-  );
+  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
 
-  addedMessage.classList.add('added-to-cart-visible');
+  addedMessage.classList.add("added-to-cart-visible");
 
-  if(addedMessageTimeoutId) {
+  if (addedMessageTimeoutId) {
     clearTimeout(addedMessageTimeoutId);
   }
 
   const timeoutId = setTimeout(() => {
-    addedMessage.classList.remove('added-to-cart-visible');
+    addedMessage.classList.remove("added-to-cart-visible");
   }, 2000);
 
   addedMessageTimeoutId = timeoutId;
-  
+
   const quantitySelector = document.querySelector(
     `.js-quantity-selector-${productId}`
   );
@@ -60,7 +61,7 @@ export function addToCart(productId) {
     cart.push({
       productId,
       quantity,
-      deliveryOptionId: '1'
+      deliveryOptionId: "1",
     });
   }
 
@@ -117,4 +118,16 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
+}
+
+export function loadCart(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    console.log(xhr.response);
+    fun();
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/cart");
+  xhr.send();
 }
