52a8510813cc9994d479ea631e862d09a71f9eef
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-07-01 13:56:55 +0300

    Convert from promises to async await

--- AmazonProject/scripts/checkout.js
+++ AmazonProject/scripts/checkout.js
@@ -6,19 +6,34 @@ import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 // import "../data/backend-practice.js";
 
-Promise.all([
-  loadProductsFetch(),
-  new Promise((resolve) => {
+async function loadPage() {
+  await loadProductsFetch();
+
+  const value = await new Promise((resolve) => {
     loadCart(() => {
-      resolve();
+      resolve("value3");
     });
-  }),
-]).then((values) => {
-  console.log(values);
+  });
+
   renderCheckoutHeader();
   renderOrderSummary();
   renderPaymentSummary();
-});
+}
+loadPage();
+
+// Promise.all([
+//   loadProductsFetch(),
+//   new Promise((resolve) => {
+//     loadCart(() => {
+//       resolve();
+//     });
+//   }),
+// ]).then((values) => {
+//   console.log(values);
+//   renderCheckoutHeader();
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
 
 // new Promise((resolve) => {
 //   loadProducts(() => {
