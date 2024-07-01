5619197f6b49190a5b247a1a46b2b347cfb66e9e
Нікулін Артем Дмитрович <48065996+lerobait@users.noreply.github.com>
2024-07-01 16:28:27 +0300

    18m

--- HomeWork/Lesson18-Ex-Backend-Promises-Async-Await/18h-r/data/cart.js
+++ HomeWork/Lesson18-Ex-Backend-Promises-Async-Await/18h-r/data/cart.js
@@ -27,27 +27,32 @@ function saveToStorage() {
 
 export function addToCart(productId) {
   let matchingItem;
-
   let addedMessageTimeoutId;
 
-  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+  if (window.location.pathname.includes("amazon.html")) {
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
 
-  addedMessage.classList.add("added-to-cart-visible");
+    if (addedMessage) {
+      addedMessage.classList.add("added-to-cart-visible");
 
-  if (addedMessageTimeoutId) {
-    clearTimeout(addedMessageTimeoutId);
+      if (addedMessageTimeoutId) {
+        clearTimeout(addedMessageTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+        addedMessage.classList.remove("added-to-cart-visible");
+      }, 2000);
+
+      addedMessageTimeoutId = timeoutId;
+    }
   }
-
-  const timeoutId = setTimeout(() => {
-    addedMessage.classList.remove("added-to-cart-visible");
-  }, 2000);
-
-  addedMessageTimeoutId = timeoutId;
 
   const quantitySelector = document.querySelector(
     `.js-quantity-selector-${productId}`
   );
-  const quantity = Number(quantitySelector.value);
+  const quantity = quantitySelector ? Number(quantitySelector.value) : 1; // Используем 1 по умолчанию, если элемента нет
 
   cart.forEach((cartItem) => {
     if (productId === cartItem.productId) {
